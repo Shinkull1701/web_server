9ed908c71ec04a02378a6484c107b970d7ba940d
Shinkull1701 <minhphuocit1701@gmail.com>
2024-06-10 11:38:26 +0700

    change model

--- src/main/java/com/shinkull/model/WebOrderQuantities.java
+++ src/main/java/com/shinkull/model/WebOrderQuantities.java
@@ -9,57 +9,92 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+/**
+ * The quantity ordered of a product.
+ */
 @Entity
 @Table(name = "web_order_quantities")
 public class WebOrderQuantities {
 
+    /** The unqiue id of the order quantity. */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id",nullable = false)
+    @Column(name = "id", nullable = false)
     private Long id;
-
+    /** The product being ordered. */
     @ManyToOne(optional = false)
-    @JoinColumn(name="product_id",nullable = false)
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
-
-    @Column (name="quantity",nullable = false)
+    /** The quantity being ordered. */
+    @Column(name = "quantity", nullable = false)
     private Integer quantity;
-
+    /** The order itself. */
     @ManyToOne(optional = false)
-    @JoinColumn(name="order_id",nullable = false)
+    @JoinColumn(name = "order_id", nullable = false)
     private WebOrder order;
-    
-    
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
+    /**
+     * Gets the order.
+     * @return The order.
+     */
     public WebOrder getOrder() {
         return order;
     }
 
+    /**
+     * Sets the order.
+     * @param order The order.
+     */
     public void setOrder(WebOrder order) {
         this.order = order;
+    }
+
+    /**
+     * Gets the quantity ordered.
+     * @return The quantity.
+     */
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * Sets the quantity ordered.
+     * @param quantity The quantity.
+     */
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * Gets the product ordered.
+     * @return The product.
+     */
+    public Product getProduct() {
+        return product;
+    }
+
+    /**
+     * Sets the product.
+     * @param product The product.
+     */
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    /**
+     * Gets the id.
+     * @return The id.
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Sets the id.
+     * @param id The id.
+     */
+    public void setId(Long id) {
+        this.id = id;
     }
 
 }
